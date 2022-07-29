b5a10a8b83ae5e460e787815a6c71b0abf7359f0
kspohn903 <mlpfanatic903@gmail.com>
2022-07-29 17:33:43 -0500

    M4tr1x Reloaded: No Exit

---
+++ ntp_syncer.py
@@ -0,0 +1,14 @@
+from time import ctime
+import ntplib
+import time
+import os
+
+try:
+    client = ntplib.NTPClient()
+    response = client.request('192.168.10.0') #IP of linux-bay server
+    print(response)
+    os.system('date %s' % (time.strftime('%m%d%H%M%Y.%S',time.localtime(response.tx_time)) ) )
+except:
+    print('Could not sync with time server.')
+
+print('Done.')
